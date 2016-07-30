afa3f382deb8458d441b4f8d014cea63003ba11b
YarikShadow <shadow_xx@ukr.net>
2016-07-30 16:43:57 +0300

    Create base project structure

---
+++ Advicer/app/src/main/java/com/imagineideas/advicer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.imagineideas.advicer;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ярослав on 30.07.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
